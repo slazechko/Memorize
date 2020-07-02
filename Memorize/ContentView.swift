5091827a8fd83a3c9659c71b455591d22d968105
Sam Lazechko <slazechko@gmail.com>
2020-07-01 22:13:05 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Sam Lazechko on 7/1/20.
+//  Copyright © 2020 Sam Lazechko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
